6c44fd0953167fa2bd0befbecb9942e4ce53216a
Itamar <iaxelrad@gmail.com>
2024-04-19 20:07:53 +0300

    List View UI completed

--- Appetizers-SwiftUI/AppetizerListView.swift
+++ Appetizers-SwiftUI/AppetizerListView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct AppetizerListView: View {
     var body: some View {
         NavigationView {
-            Text("Appetizer List View")
-                .navigationTitle("🍟 Appetizers")
+            List(MockData.appetizers) { appetizer in
+                AppetizerListCell(appetizer: appetizer)
+            }
+            .navigationTitle(("🍟 Appetizers"))
         }
     }
 }
@@ -19,4 +21,6 @@ struct AppetizerListView: View {
 #Preview {
     AppetizerListView()
 }
+
+
 
