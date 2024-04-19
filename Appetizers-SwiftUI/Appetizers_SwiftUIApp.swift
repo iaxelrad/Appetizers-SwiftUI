68e2c6140d535bb4da6d94fa8ba936bb7c2531d3
Itamar <iaxelrad@gmail.com>
2024-04-19 17:33:28 +0300

    Initial Commit

---
+++ Appetizers-SwiftUI/Appetizers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appetizers_SwiftUIApp.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Itamar Axelrad on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Appetizers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
