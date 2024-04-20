dcdad1a8882e8c6b42d3ada7a47f5b1c3cff2997
Itamar <iaxelrad@gmail.com>
2024-04-20 12:13:16 +0300

    LoadingView added

---
+++ Appetizers-SwiftUI/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers-SwiftUI
+//
+//  Created by Itamar Axelrad on 20/04/2024.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimaryOriginal")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimaryOriginal")
+}
