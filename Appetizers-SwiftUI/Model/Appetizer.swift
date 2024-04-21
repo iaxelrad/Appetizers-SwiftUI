98aaedd38277210e27e0d1e868e904b5cd0cca64
Itamar <iaxelrad@gmail.com>
2024-04-21 10:51:06 +0300

    Order Screen UI completed

--- Appetizers-SwiftUI/Model/Appetizer.swift
+++ Appetizers-SwiftUI/Model/Appetizer.swift
@@ -33,4 +33,33 @@ struct MockData {
                                            carbs: 99)
 
     static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+
+    static let orderItemOne = Appetizer(id: 1,
+                                        name: "Test Appetizer 1",
+                                        description: "This is the description for appetizer 1.",
+                                        price: 8.99,
+                                        imageURL: "",
+                                        calories: 99,
+                                        protein: 99,
+                                        carbs: 99)
+
+    static let orderItemTwo = Appetizer(id: 2,
+                                        name: "Test Appetizer 2",
+                                        description: "This is the description for appetizer 2.",
+                                        price: 8.99,
+                                        imageURL: "",
+                                        calories: 99,
+                                        protein: 99,
+                                        carbs: 99)
+
+    static let orderItemThree = Appetizer(id: 3,
+                                        name: "Test Appetizer 3",
+                                        description: "This is the description for appetizer 3.",
+                                        price: 8.99,
+                                        imageURL: "",
+                                        calories: 99,
+                                        protein: 99,
+                                        carbs: 99)
+
+    static let orderItems = [orderItemOne, orderItemTwo, orderItemThree]
 }
--- Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
+++ Appetizers-SwiftUI/Screens/OrderView/OrderView.swift
@@ -8,18 +8,41 @@
 import SwiftUI
 
 struct OrderView: View {
+
+    @State private var orderItems = MockData.orderItems
     var body: some View {
         NavigationView {
-            Text("Orders")
-                .navigationTitle("🧾 Orders")
+            VStack {
+                List {
+                    ForEach(orderItems) { appetizer in
+                        AppetizerListCell(appetizer: appetizer)
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+                .listStyle(PlainListStyle())
+
+                Button {
+                    print("order placed")
+                } label: {
+                    APButton(title: "$99.99 - Place Order")
+                }
+                .padding(.bottom, 25)
+            }
+            .navigationTitle("🧾 Orders")
         }
+
         .enableInjection()
+    }
+
+    func deleteItems(at offsets: IndexSet) {
+        orderItems.remove(atOffsets: offsets)
     }
 
     #if DEBUG
     @ObserveInjection var forceRedraw
     #endif
 }
+
 #Preview {
     OrderView()
 }
